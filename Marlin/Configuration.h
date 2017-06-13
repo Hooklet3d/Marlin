932c726925282746cb96f8c8d4c90a0847d2b4e6
Steve Griffin <Griffin@Hooklet3d.com>
2017-06-12 20:13:52 -0500

    Add Filament Change
    Added Filament Change and adjusted minor settings

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -583,7 +583,7 @@
 //  (0,0)
 #define X_PROBE_OFFSET_FROM_EXTRUDER 50  // X offset: -left  +right  [of the nozzle]
 #define Y_PROBE_OFFSET_FROM_EXTRUDER 54  // Y offset: -front +behind [the nozzle]
-#define Z_PROBE_OFFSET_FROM_EXTRUDER -0.12   // Z offset: -below +above  [the nozzle]
+#define Z_PROBE_OFFSET_FROM_EXTRUDER -0.95   // Z offset: -below +above  [the nozzle]
 
 // X and Y axis travel speed (mm/m) between probes
 #define XY_PROBE_SPEED 8000
